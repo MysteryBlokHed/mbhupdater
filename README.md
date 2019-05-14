f5a3964d6d55b739280624a6baa323d4c3f21383
MysteryBlokHed <adamthompsonsharpe@gmail.com>
2019-05-13 20:58:33 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# MBH Updater
+A Python script designed for frontend updating of files.
+
+## Overview
+This is a fairly simple program that will copy files from online and keep the file structure. For example, if you have `/path/to/file/code.txt` and `/path/to/code.txt`, it will keep those files in those folders locally.
+
+## Requirements
+**Python v3+**  
+If you don't already have it, download it [here](https://www.python.org/downloads/).
+
+## Documentation
+### Updater class
+What each class variable means.
+
+    class Updater(object):
+
+    def __init__(local_version_file="version.txt",
+                latest_version_file="new_version.txt",
+                latesturl=False, new_files=[],
+                new_filesurl=True)
+        """
+        local_version_file: The downloaded version of the software.
+        Should contain a number, like 1, 1.2, 1.234.
+        latest_version_file: Likely stored online. This is the latest version of the software.
+        Should contain a number, like 2, 2.3, 2.345.
+        latesturl: Boolean. Whether or not the file stored at latest_version_file is a url.
+        new_files: Array. Contains the location of the files to update if the versions don't match.
+        new_filesurl: Boolean. Whether or not the items listed in new_files are urls.
+        """
+
+### Initialize the updater
+    import mbhupdater.updater
+
+    updater = mbh.updater.Updater()
+
+### Pull the new files
+Pull without comparing versions:  
+    `updater.pull_files()`  
+Pull if local version is outdated:  
+    `updater.compare_and_pull()`  
+
+### Getters and setters
+Pretty easy to guess what they do, they just use the variable names in the functions. Here's a list:
+
+    # Getters
+    get_local_version_file()
+    get_latest_version_file()
+    get_latesturl_status()
+    get_new_filesurl_status()
+    get_local_version()
+    get_latest_version()
+    # Setters
+    set_local_version_file(local_version_file)
+    set_latest_version_file(latest_version_file)
+    toggle_latesturl()
+    toggle_new_filesurl()
+All functions have descriptions that will show up in IDEs.
+
+### Other functions
+        # Other functions
+        compare_versions() - Returns True if the versions match and False if the local one is out of date.
+        read_files()       - Reads the content of the version files. Must use before get_local_version() or get_latest_version()
---
+++ mbhupdater/updater.py
@@ -0,0 +1,186 @@
+# Created by MysteryBlokHed in 2019.
+import urllib.request
+import os
+
+class Updater(object):
+    # Location of the local version file
+    _local_version_file = ""
+    # Location of the latest version file
+    _latest_version_file = ""
+    # If the latest update file is online or not
+    _latesturl = False
+
+    _local_version = ""
+    _latest_version = ""
+
+    _new_files = []
+
+    _new_filesurl = True
+
+    def __init__(self, local_version_file="version.txt", latest_version_file="new_version.txt", latesturl=False, new_files=[], new_filesurl=True):
+        # Verify the type of local_version_file
+        if type(local_version_file) is str:
+            self._local_version_file = local_version_file
+        else:
+            raise TypeError("Expected string for local_version_file, got {}.".format(type(local_version_file)))
+        # Verify the type of latest_version_file
+        if type(latest_version_file) is str:
+            self._latest_version_file = latest_version_file
+        else:
+            raise TypeError("Expected string for latest_version_file, got {}.".format(type(latest_version_file)))
+        # Verify the type of latesturl
+        if type(latesturl) is bool:
+            self._latesturl = latesturl
+        else:
+            raise TypeError("Expected boolean for latesturl, got {}.".format(type(latesturl)))
+        # Verify the type of new_files
+        if type(new_files) is list:
+            self._new_files = new_files
+        else:
+            raise TypeError("Expected list for new_files, got {}.".format(type(new_files)))
+        # Verify the type of new_filesurl
+        if type(new_filesurl) is bool:
+            self._new_filesurl = new_filesurl
+        else:
+            raise TypeError("Expected boolean for new_filesurl, got {}.".format(type(new_filesurl)))
+    
+    def get_local_version_file(self):
+        """Returns the location of the local version file."""
+        return self._local_version_file
+    
+    def get_latest_version_file(self):
+        """Returns the location of the latest version file."""
+        return self._latest_version_file
+    
+    def get_latesturl_status(self):
+        """Returns a boolean of whether or not latest_version_file contains a URL."""
+        return self._latesturl
+    
+    def get_new_filesurl_status(self):
+        """Returns a boolean of whether or not the new files that (might) need copying are URLs."""
+        return self._new_filesurl
+    
+    def get_local_version(self):
+        """Returns the version of the local files.
+        Must use read_files() first."""
+        return self._local_version
+    
+    def get_local_version(self):
+        """Returns the version of the latest files.
+        Must use read_files() first."""
+        return self._latest_version
+    
+    def set_local_version_file(self, local_version_file):
+        """Set the location of the local version file."""
+        # Verify the type of local_version_file
+        if type(local_version_file) is str:
+            self._local_version_file = local_version_file
+        else:
+            raise TypeError("Expected string for local_version_file, got {}.".format(type(local_version_file)))
+    
+    def set_latest_version_file(self, latest_version_file):
+        """Set the location of the latest version file."""
+        # Verify the type of latest_version_file
+        if type(latest_version_file) is str:
+            self._latest_version_file = latest_version_file
+        else:
+            raise TypeError("Expected string for latest_version_file, got {}.".format(type(latest_version_file)))
+    
+    def toggle_latesturl(self):
+        """Toggles the boolean of whether or not latest_version_file contains a URL."""
+        # Simple toggle for booleans
+        self._latesturl = not self._latesturl
+    
+    def toggle_new_filesurl(self):
+        """Toggles the boolean of whether or not the new files that (might) need copying are URLs."""
+        # Simple toggle for booleans
+        self._new_filesurl = not self._new_filesurl
+        
+        self._local_version = local.readlines()
+        local.close()
+    
+    def read_files(self):
+        """Reads the version files to ready them for comparison.
+        Not neccessary unless you need to use the get_local_version() or get_latest_version() functions."""
+        local = open(self._local_version_file, "r")
+        
+        # Does different things if the latest version file is a url or not.
+        if self._latesturl:
+            latest = urllib.request.urlopen(self._latest_version_file)
+            self._latest_version = latest.read()
+        else:
+            latest = open(self._latest_version_file, "r")
+            self._latest_version = latest.readlines()
+            latest.close()
+        
+        self._local_version = local.readlines()
+        local.close()
+    
+    def compare_versions(self):
+        """Returns True if the versions match, and False if they don't. Throws an error if the local version is newer."""
+        if float(self._latest_version) == float(self._local_version):
+            return True
+        elif float(self._latest_version) > float(self._local_version):
+            return False
+        else:
+            raise ValueError("Local version is newer than the latest version listed, consider updating the latest version file.")
+
+    def pull_files(self):
+        """Not recommended unless you don't want to update the version file locally.
+        Pulls all files to the working directory."""
+        # Does different things if the new files are online or not.
+        if self._new_filesurl:
+            print("Files are online.")
+            i = 0
+            # For each new file in array
+            for item in self._new_files:
+                # Set the target location to output to
+                fname = item.split("/", 3)[-1:][0]
+                # Create the directories for the file if they don't exist
+                os.makedirs(os.path.dirname(fname), exist_ok=True)
+                # Read the file line-by-line
+                lines = urllib.request.urlopen(self._new_files[i]).readlines()
+                print("Read file from {}".format(fname))
+                # Decode.
+                j = 0
+                for line in lines:
+                    try:
+                        lines[j] = line.decode("utf-8")
+                        j+=1
+                        print("Decoded line {} of {}.".format(j+1, fname), end="\r")
+                    # If it's already a string
+                    except AttributeError:
+                        pass
+                # Write the contents of the new file to the working directory.
+                print("\nWriting file from {}...".format(fname))
+                f = open(fname, "w+")
+                f.writelines(lines)
+                print("Wrote file {}.".format(fname))
+                i+=1
+        else:
+            print("Files are offline.")
+            i = 0
+            # For each new file in array
+            for item in self._new_files:
+                fname = item.split("/")[-1:][0]
+                # Read the file line-by-line
+                lines = open(item, "r").readlines()
+                print("Read file from {}".format(fname))
+                # Write the contents of the new file to the working directory.
+
+                f = open(fname, "w+")
+                f.writelines(lines)
+                f.close()
+    
+    def compare_and_pull(self):
+        """Compares the versions. If the current version is older, it will update the current files."""
+        self.read_files()
+
+        if not self.compare_versions():
+            print("Outdated version detected. Updating...")
+            self.pull_files()
+            f = open(self._local_version_file, "w+")
+            f.writelines(self._latest_version)
+            f.close()
+        else:
+            print("Files are up to date.")
