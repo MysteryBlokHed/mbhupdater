5e3bde5e636f59c9353e940cb0dc2b507894af9f
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2019-08-14 15:08:09 -0400

    Added checking of output variable for TQDMUpdater (Issue #4).

--- mbhupdater/tqdm_updater.py
+++ mbhupdater/tqdm_updater.py
@@ -36,7 +36,8 @@ class TQDMUpdater(mbhupdater.updater.Updater):
         files_to_delete = []
         # Does different things if the new files are online or not.
         if self._new_filesurl:
-            tqdm.write("Files are online.")
+            if self._output:
+                tqdm.write("Files are online.")
             # For each new file in array
             for i in tqdm(range(len(self._new_files))):
                 if self._new_files[i][:8] == "[DELETE]":
@@ -50,7 +51,8 @@ class TQDMUpdater(mbhupdater.updater.Updater):
                     os.makedirs(os.path.dirname(fname), exist_ok=True)
                     # Read the file line-by-line
                     lines = urllib.request.urlopen(self._new_files[i]).readlines()
-                    tqdm.write(f"Read file from {fname}.")
+                    if self._output:
+                        tqdm.write(f"Read file from {fname}.")
                     # Whether or not the file is stored as bytes
                     b = False
                     # Sometimes breaks, but from testing it seems fine to skip.
@@ -60,48 +62,57 @@ class TQDMUpdater(mbhupdater.updater.Updater):
                     except:
                         pass
                     # Write the contents of the new file to the working directory.
-                    tqdm.write(f"Writing file {fname}...")
+                    if self._output:
+                        tqdm.write(f"Writing file {fname}...")
                     # Opens the file differently if it is of type bytes.
                     if b:
                         f = open(fname, "wb+")
                     else:
                         f = open(fname, "w+")
                     f.writelines(lines)
-                    tqdm.write(f"Wrote file {fname}.")
+                    if self._output:
+                        tqdm.write(f"Wrote file {fname}.")
                 else:
                     if not self._new_files[i][:8] == "[DELETE]":
                         files_to_delete.append(self._new_files[i])
         else:
-            tqdm.write("Files are offline.")
+            if self._output:
+                tqdm.write("Files are offline.")
             # For each new file in array
             for i in tqdm(range(len(self._new_files))):
                 fname = self._new_files[i].split("/")[-1:][0]
                 # Read the file line-by-line
                 lines = open(self._new_files[i], "r").readlines()
-                tqdm.write(f"Read file from {fname}.")
+                if self._output:
+                    tqdm.write(f"Read file from {fname}.")
                 # Whether or not the file is stored as bytes
                 b = False
                 if type(lines[0]) is bytes:
                     b = True
                 # Write the contents of the new file to the working directory.
-                tqdm.write(f"Writing file from {fname}...")
+                if self._output:
+                    tqdm.write(f"Writing file from {fname}...")
                 # Opens the file differently if it is of type bytes.
                 if b:
                     f = open(fname, "wb+")
                 else:
                     f = open(fname, "w+")
                 f.writelines(lines)
-                tqdm.write(f"Wrote file {fname}.")
+                if self._output:
+                    tqdm.write(f"Wrote file {fname}.")
         
         # Delete the files marked
-        tqdm.write("Deletinng old files...")
+        if self._output:
+            tqdm.write("Deletinng old files...")
         for i in tqdm(range(len(files_to_delete))):
             # Check if the item is a file or folder
             if files_to_delete[i].strip("\n").strip("\r")[-1] == "/": # Remove line endings to check last character
                 if os.path.isdir(files_to_delete[i].strip("\n").strip("\r").strip("/")):
-                    tqdm.write(f"Deleting folder {files_to_delete[i]}...")
+                    if self._output:
+                        tqdm.write(f"Deleting folder {files_to_delete[i]}...")
                     shutil.rmtree(files_to_delete[i].strip("\n").strip("\r").strip("/"))
             else:
                 if os.path.isfile("./"+files_to_delete[i]):
-                    tqdm.write(f"Deleting file {files_to_delete[i]}...")
+                    if self._output:
+                        tqdm.write(f"Deleting file {files_to_delete[i]}...")
                     os.remove("./"+files_to_delete[i])
